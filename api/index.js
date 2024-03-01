0f3973cd212e0f525f5c43ff01bc35957b21ee11
Pritam <pritam@codewithme.com>
2024-03-02 01:06:17 +0530

    websocket connection

--- api/index.js
+++ api/index.js
@@ -2,14 +2,16 @@
 
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 //import and configure dotenv:
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 const User = require("./models/User");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+
+const ws = require("ws");
 
 dotenv.config();
 // console.log(process.env.MONGO_URL);
@@ -31,67 +33,108 @@ app.use(
   })
 );
 
-app.get('/test', (req, res) => {
+app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-app.get('/profile', (req, res) => {
+app.get("/profile", (req, res) => {
   const token = req.cookies?.token;
 
-  if(token){
+  if (token) {
     jwt.verify(token, jwtSecret, {}, (err, userData) => {
-      if(err) throw err;
+      if (err) throw err;
       res.json(userData);
     });
+  } else {
+    res.status(401).json("no token");
   }
-  else{
-    res.status(401).json('no token');
-  }
-  
 });
 
-app.post('/login', async (req, res) => {
-  const {username, password} = req.body;
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-  const foundUser = await User.findOne({username});
+  const foundUser = await User.findOne({ username });
 
-  if(foundUser){
+  if (foundUser) {
     const passOk = bcrypt.compareSync(password, foundUser.password);
 
-    if(passOk){
-      jwt.sign({ userId: foundUser._id, username}, jwtSecret, {}, (err, token) => {
-             res.cookie('token', token, {sameSite:'none', secure:true}).json({
-              id: foundUser._id,
-             })
-      });
+    if (passOk) {
+      jwt.sign(
+        { userId: foundUser._id, username },
+        jwtSecret,
+        {},
+        (err, token) => {
+          res.cookie("token", token, { sameSite: "none", secure: true }).json({
+            id: foundUser._id,
+          });
+        }
+      );
     }
   }
-
-})
+});
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const hashedPassword = bcrypt.hashSync(password, bcryptSalt)
-    const createdUser = await User.create({ 
-      username:username, 
-      password:
-      hashedPassword,
+    const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+    const createdUser = await User.create({
+      username: username,
+      password: hashedPassword,
     });
-    jwt.sign({ userId: createdUser._id, username }, jwtSecret, {}, (err, token) => {
-      if (err) throw err;
-      res.cookie("token", token, {sameSite:'none', secure:true}).status(201).json({
-        id: createdUser._id,
-      });
-    });
+    jwt.sign(
+      { userId: createdUser._id, username },
+      jwtSecret,
+      {},
+      (err, token) => {
+        if (err) throw err;
+        res
+          .cookie("token", token, { sameSite: "none", secure: true })
+          .status(201)
+          .json({
+            id: createdUser._id,
+          });
+      }
+    );
   } catch (err) {
     if (err) throw err;
     res.status(500).json("error");
   }
 });
 
-app.listen(4044);
+const server = app.listen(4044);
 
 //WebChat9437
 //RRMlFkdTuJlaOHxe
+
+//websocket connection
+const wss = new ws.WebSocketServer({ server });
+wss.on("connection", (connection, req) => {
+  // console.log('connected');
+  // console.log(req.headers);
+  const cookies = req.headers.cookie;
+
+  if (cookies) {
+    const tokenCookieString = cookies
+      .split(";")
+      .find((str) => str.startsWith("token="));
+
+    // console.log(tokenCookieString);
+
+    if (tokenCookieString) {
+      const token = tokenCookieString.split("=")[1];
+      if (token) {
+        // console.log(token);
+
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+          if (err) throw err;
+          // console.log(userData);
+          const { userId, username } = userData;
+
+          connection.userId = userId;
+          connection.username = username;
+        });
+      }
+    }
+  }
+});
