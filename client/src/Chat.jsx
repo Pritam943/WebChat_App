4187ffda9c900406cff9ff77d104e3233569fc2f
Pritam <pritam@codewithme.com>
2024-03-09 00:56:46 +0530

    selecting conversion

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Avatar from "./Avatar";
+import Logo from "./Logo";
+import { UserContext } from "./userContext";
 
 const Chat = () => {
   const [, setWs] = useState(null);
   const [onlinePeople, setOnlinePeople] = useState({});
+  const [selectedUserId, setSelectedUserId] = useState(null);
+  const { username, id } = useContext(UserContext);
 
   useEffect(() => {
     const ws = new WebSocket("ws://localhost:4044");
@@ -33,35 +37,46 @@ const Chat = () => {
     }
   }
 
+  const onlinePeopleExclOurUser = { ...onlinePeople };
+  delete onlinePeopleExclOurUser[id];
+
   return (
     <div className="flex h-screen">
-      <div className="bg-white w-1/3 pl-5 pt-4">
-        <div className="text-blue-700 text-xl font-bold flex gap-2 mb-4">
-          WebChat
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            className="w-6 h-6"
-          >
-            <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 0 0-1.032-.211 50.89 50.89 0 0 0-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 0 0 2.433 3.984L7.28 21.53A.75.75 0 0 1 6 21v-4.03a48.527 48.527 0 0 1-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979Z" />
-            <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 0 0 1.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0 0 15.75 7.5Z" />
-          </svg>
-        </div>
-        {Object.keys(onlinePeople).map((userId) => (
+      <div className="bg-white w-1/4 ">
+        <Logo />
+
+        {Object.keys(onlinePeopleExclOurUser).map((userId) => (
           <div
-            className="border-b border-gray-300 py-2 flex items-center gap-2 cursor-pointer"
-            key={onlinePeople[userId]}
+            onClick={() => setSelectedUserId(userId)}
+            className={
+              "border-b border-gray-300 flex items-center gap-2 cursor-pointer " +
+              (userId === selectedUserId ? "bg-green-200" : "")
+            }
+            key={userId}
           >
-            <Avatar username={onlinePeople[userId]} userId={userId} />
-            <span className="text-gray-800 text-xl">
-              {onlinePeople[userId]}
-            </span>
+            {userId === selectedUserId && (
+              <div className="w-1 bg-green-500 h-12 rounded-r-md"></div>
+            )}
+            <div className="flex items-center gap-2 py-2 pl-4 ">
+              <Avatar username={onlinePeople[userId]} userId={userId} />
+              <span className="text-gray-800 text-xl">
+                {onlinePeople[userId]}
+              </span>
+            </div>
           </div>
         ))}
       </div>
-      <div className="flex flex-col bg-blue-400 w-2/3 p-2">
-        <div className="flex-grow">messages with selected person</div>
+      <div className="flex flex-col bg-slate-800 w-3/4  p-2">
+        <div className="flex-grow">
+          {!selectedUserId && (
+            <div className="h-full flex items-center justify-center">
+              <div className="bg-gray-900 w-1/3 h-10 opacity-20 rounded-md flex items-center justify-center">
+                <div className="text-white text-xl">Welcome to WebChat.</div>
+              </div>
+              {/* <div>&larr;Select a person from side bar</div> */}
+            </div>
+          )}
+        </div>
 
         <div className="flex gap-2">
           <input
@@ -70,7 +85,7 @@ const Chat = () => {
             className="bg-white flex-grow boarder
             p-2 "
           />
-          <button className="bg-blue-500 p-2 text-white rounded-sm">
+          <button className="bg-green-600 p-2 text-white rounded-sm">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
